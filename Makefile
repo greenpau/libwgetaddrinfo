6d326c226fb9faeb3b1542f11e9b6a9b26d4ccdf
Paul Greenberg <greenpau@outlook.com>
2020-02-28 21:46:49 -0500

    add debug

--- Makefile
+++ Makefile
@@ -1,13 +1,18 @@
-.PHONY: clean install
+.PHONY: clean install test
 LIB_NAME=libwgetaddrinfo
 LIB_PATH=/usr/local/lib/libwgetaddrinfo/lib
 
-all: clean libs
-
-libs: getaddrinfo_w.c
-	gcc -D_GNU_SOURCE -fPIC -lm -ldl -shared -Wl,-soname,$(LIB_NAME).so.1 -o $(LIB_NAME).so.1.0 getaddrinfo_w.c
+all: clean getaddrinfo_w.c
+	echo "Building"
+	mkdir -p lib
+	#gcc -DDEBUG -D_GNU_SOURCE -fPIC -lm -ldl -shared -Wl,-soname,$(LIB_NAME).so.1 -o ./lib/$(LIB_NAME).so.1.0 getaddrinfo_w.c
+	gcc -D_GNU_SOURCE -fPIC -lm -ldl -shared -Wl,-soname,$(LIB_NAME).so.1 -o ./lib/$(LIB_NAME).so.1.0 getaddrinfo_w.c
+	cd lib && ln -s `pwd`/$(LIB_NAME).so.1.0 `pwd`/$(LIB_NAME).so.1
+	cd lib && ln -s `pwd`/$(LIB_NAME).so.1 `pwd`/$(LIB_NAME).so
 
 clean:
+	@echo "Performing cleanup"
+	rm -rf ./lib/
 	rm -f *.o
 	rm -f $(LIB_NAME).so*
 
@@ -17,3 +22,8 @@ install:
 	sudo cp $(LIB_NAME).so.1.0 $(LIB_PATH)/
 	sudo ln -s $(LIB_PATH)/$(LIB_NAME).so.1.0 $(LIB_PATH)/$(LIB_NAME).so.1 2>/dev/null
 	sudo ln -s $(LIB_PATH)/$(LIB_NAME).so.1 $(LIB_PATH)/$(LIB_NAME).so 2>/dev/null
+
+test:
+	#LD_PRELOAD=`pwd`/lib/$(LIB_NAME).so strace nslookup www.google.com
+	#LD_PRELOAD=`pwd`/lib/$(LIB_NAME).so strace dig www.google.com ANY
+	LD_PRELOAD=`pwd`/lib/$(LIB_NAME).so dig www.google.com AAAA
--- getaddrinfo_w.c
+++ getaddrinfo_w.c
@@ -6,6 +6,7 @@
  * <include GPL>
  */
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <dlfcn.h>
 #include <sys/types.h>
@@ -22,6 +23,9 @@ int getaddrinfo (const char *node, const char *service,
 		const struct addrinfo *hints,
 		struct addrinfo **res)
 {
+#ifdef DEBUG
+    fprintf(stderr, MODPROMPT "Received getaddrinfo():\n");
+#endif
 	int getaddr_ret;
 	char *errval;
 	struct addrinfo _nhints;
@@ -36,19 +40,25 @@ int getaddrinfo (const char *node, const char *service,
 
 	/* Wokaround wrt RFC2553, if AF_UNSPEC is set, we will lookup
 	   both IPv4 and IPv6. Some software can do this, and some routers
-           won't allow AAAA DNS requests. */
+        won't allow AAAA DNS requests. */
+
 	if (hints != NULL) {
-		if ((hints->ai_family == AF_UNSPEC)) {
-			_nhints = *hints;
-			_nhints.ai_family = AF_INET;
-			if (!(hints->ai_flags & AI_ADDRCONFIG)) {
-				_nhints.ai_flags |= AI_ADDRCONFIG;
-			}
-		}
+        if ((hints->ai_family == AF_UNSPEC)) {
+	        _nhints = *hints;
+		    _nhints.ai_family = AF_INET;
+		    if (!(hints->ai_flags & AI_ADDRCONFIG)) {
+			    _nhints.ai_flags |= AI_ADDRCONFIG;
+		    }
+        }
 	} 
 	else	
 		nhints = hints;
 
+#ifdef DEBUG
+    fprintf(stderr, MODPROMPT "node: %s.\n", node);
+    fprintf(stderr, MODPROMPT "service: %s.\n", service);
+    fprintf(stderr, MODPROMPT "hints: %s.\n", hints);
+    fprintf(stderr, MODPROMPT "ai family: %x\n", &hints->ai_family);
+#endif
 	return libc_getaddrinfo(node, service, nhints, res);
 }
-
